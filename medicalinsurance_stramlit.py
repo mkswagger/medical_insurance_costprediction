113dc73099beb955a8b424245c0cf76ad0d39fba
mkswagger <mathangykrishna16@gmail.com>
2023-02-26 20:46:48 +0530

    Update medicalinsurance_stramlit.py

--- medicalinsurance_stramlit.py
+++ medicalinsurance_stramlit.py
@@ -5,30 +5,38 @@ Created on Sun Feb 26 20:00:09 2023
 @author: MK$WAGGER
 """
 
-import pickle
-import numpy as np
+
+import numpy as np 
+import pickle 
 import streamlit as st
 
-loaded_model= pickle.load(open("G:\ONEDRIVE\Documents\GitHub\medical_insurance_costprediction\Trained_model.sav",'rb'))
-def insurance(input_data):
-    input_data_as_np_array = np.array(input_data)
-    input_data_reshape = input_data_as_np_array.reshape(-1,1)
+loaded_model = pickle.load(open("Trained_model.sav","rb"))
+
+def health_insurance_cost(input_data):
+    input_data_as_np_array = np.asarray(input_data)
+    input_data_reshaped = input_data_as_np_array.reshape(1,-1)
     
-    prediction = loaded_model.predict(input_data_reshape)
+    prediction = loaded_model.predict(input_data_reshaped)
     print(prediction)
     
- 
+    return prediction
+    
+
 def main():
-    st.title("Medical Health Insurance Predictor App")
+    st.title("Health Insurance Cost Prediction Web App")
+    
+    # getting input from the user
+    
     Age = st.text_input('Age')
     Sex = st.text_input('Sex (1->Male, 0->Female)')
-    bmi = st.text_input('BMI')
-    children = st.text_input("No of children:")
+    bmi = st.text_input("BMI")
+    children = st.text_input("No of children")
     smoker = st.text_input("Smoker or not (0->non smoker, 1 -> non smoker")
+  # code for prediction 
     cost = ""
     
     if st.button("Test Results : "):
-        cost = insurance([Age,Sex,bmi,children,smoker])
+        cost = health_insurance_cost([Age,Sex,bmi,children,smoker])
     
     st.success(cost)
 
